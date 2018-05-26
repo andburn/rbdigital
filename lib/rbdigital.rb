eae2cfc1650aa058900f89730c7d6f5004c402f7
Andrew Burnett <andrew@andburn.info>
2018-05-26 12:36:56 +0100

    Remove data holding classes

--- lib/rbdigital.rb
+++ lib/rbdigital.rb
@@ -1,4 +1,18 @@
-module Rbdigital; end
+module Rbdigital
+  extend self
+
+  def get_magazine_ids(library_id)
+  end
+
+  def get_magazine(library_id, magazine_id)
+  end
+
+  def checkout_magazines(library_id, user_name, user_pass, *magazine_ids)
+  end
+
+  def return_magazines(library_id, user_name, user_pass, *magazine_ids)
+  end
+end
 
 require "rbdigital/version"
 require "rbdigital/application"
