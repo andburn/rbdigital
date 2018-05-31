c045852f95ef6a35d730d8f4a333781c41e7d94c
Andrew Burnett <andrew@andburn.info>
2018-05-31 21:38:48 +0100

    Refactor magazine spec

--- spec/magazine_spec.rb
+++ spec/magazine_spec.rb
@@ -7,43 +7,35 @@ describe 'magazine' do
 
 	describe 'get_info' do
     it 'period should be zero if a single issue' do
-      stub_request(:get, @mag_url).
-        with(:headers => {'Accept'=>'*/*', 'Cookie'=>'', 'User-Agent'=>'Ruby'}).
-        to_return(:body => get_data_file('one_issue_only.html'), :status => 200)
-      @magazine.get_info
+      get_info_stub("one_issue_only")
       expect(@magazine.period).to eq 0
     end
 
     it 'period should be four if monthly' do
-      stub_request(:get, @mag_url).
-        with(:headers => {'Accept'=>'*/*', 'Cookie'=>'', 'User-Agent'=>'Ruby'}).
-        to_return(:body => get_data_file('current_issue.html'), :status => 200)
-      @magazine.get_info
+      get_info_stub("current_issue")
       expect(@magazine.period).to eq 4
     end
 
     it 'period should be one if weekly' do
-      stub_request(:get, @mag_url).
-        with(:headers => {'Accept'=>'*/*', 'Cookie'=>'', 'User-Agent'=>'Ruby'}).
-        to_return(:body => get_data_file('weekly_issue.html'), :status => 200)
-      @magazine.get_info
+      get_info_stub("weekly_issue")
       expect(@magazine.period).to eq 1
     end
 
     it 'should be true if only back issues are available' do
-      stub_request(:get, @mag_url).
-        with(:headers => {'Accept'=>'*/*', 'Cookie'=>'', 'User-Agent'=>'Ruby'}).
-        to_return(:body => get_data_file('back_issues_only.html'), :status => 200)
-      @magazine.get_info
+      get_info_stub("back_issues_only")
       expect(@magazine.archived?).to be_truthy
     end
 
     it 'should be false if current issue is available' do
-      stub_request(:get, @mag_url).
-        with(:headers => {'Accept'=>'*/*', 'Cookie'=>'', 'User-Agent'=>'Ruby'}).
-        to_return(:body => get_data_file('current_issue.html'), :status => 200)
-      @magazine.get_info
+      get_info_stub("current_issue")
       expect(@magazine.archived?).to be_falsey
     end
+  end
+
+  def get_info_stub(file)
+    stub_request(:get, @mag_url).
+        with(:headers => {'Accept'=>'*/*', 'Cookie'=>'', 'User-Agent'=>'Ruby'}).
+        to_return(:body => get_data_file("#{file}.html"), :status => 200)
+      @magazine.get_info
   end
 end
