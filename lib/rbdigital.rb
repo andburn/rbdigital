6ef2a02752310919aaed0d9c2f8cb75a2d87986a
Andrew Burnett <andrew@andburn.info>
2018-06-19 12:23:18 +0100

    Add module level Library object

--- lib/rbdigital.rb
+++ lib/rbdigital.rb
@@ -7,7 +7,19 @@ module Rbdigital
   # set global logger base level
   Log4r::Logger.root.level = Log4r::WARN
 
+  @@library = nil
   @@logger = nil
+
+  def self.library
+    if @@library.nil?
+      raise LibraryError.new("library object is nil")
+    end
+    return @@library
+  end
+
+  def self.set_library(code, id)
+    @@library = Library.new(code, id)
+  end
 
   def self.logger
     if @@logger.nil?
@@ -19,13 +31,18 @@ module Rbdigital
   end
 
   def self.log_to_file(log_file, level=Log4r::INFO)
-    # add a file outputter to logger if arg is ok
-    if not log_file.nil? && Dir.exist?(File.dirname(log_file))
-      file_output = Log4r::FileOutputter.new("logfile",
-        :filename => log_file,
-        :trunc => false,
-        :level => level)
-      self.logger.add(file_output)
+    name = "logfile"
+    if Log4r::Outputter[name].nil?
+      # add a file outputter to logger if arg is ok
+      if not log_file.nil? && Dir.exist?(File.dirname(log_file))
+        file_output = Log4r::FileOutputter.new(name,
+          :filename => log_file,
+          :trunc => false,
+          :level => level)
+        self.logger.add(file_output)
+      end
+    else
+      self.logger.warn 'logfile outputter already exists'
     end
   end
 end
